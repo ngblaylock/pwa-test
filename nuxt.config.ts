72bf71b369cb6db2673de9e371dde1736fb7ca5f
Nathan Blaylock <ngblaylock@gmail.com>
2024-02-04 00:51:52 -0700

    build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,11 +27,13 @@ export default defineNuxtConfig({
           src: "icons/192.png",
           sizes: "192x192",
           type: "image/png",
+          purpose: "maskable",
         },
         {
           src: "icons/512.png",
           sizes: "512x512",
           type: "image/png",
+          purpose: "maskable",
         },
       ],
     },
