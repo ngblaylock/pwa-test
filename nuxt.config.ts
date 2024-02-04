e11c96b14a08091a7cca7eb28247823998292bcd
Nathan Blaylock <ngblaylock@gmail.com>
2024-02-04 00:25:33 -0700

    update docs directory

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  buildDir: 'docs',
+  nitro: {
+    output: {
+      publicDir: "docs",
+    },
+  },
   modules: ["@vite-pwa/nuxt"],
   pwa: {
     manifest: {
@@ -10,26 +14,26 @@ export default defineNuxtConfig({
       description: "This is a test",
       icons: [
         {
-          src: 'icons/64.png',
-          sizes: '64x64',
-          type: 'image/png',
+          src: "icons/64.png",
+          sizes: "64x64",
+          type: "image/png",
         },
         {
-          src: 'icons/144.png',
-          sizes: '144x144',
-          type: 'image/png',
+          src: "icons/144.png",
+          sizes: "144x144",
+          type: "image/png",
         },
         {
-          src: 'icons/192.png',
-          sizes: '192x192',
-          type: 'image/png',
+          src: "icons/192.png",
+          sizes: "192x192",
+          type: "image/png",
         },
         {
-          src: 'icons/512.png',
-          sizes: '512x512',
-          type: 'image/png',
+          src: "icons/512.png",
+          sizes: "512x512",
+          type: "image/png",
         },
-      ]
+      ],
     },
     workbox: {
       navigateFallback: "/",
