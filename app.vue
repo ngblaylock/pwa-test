9671865c552ac7a07adba2be0faa5d5050412cda
Nathan Blaylock <ngblaylock@gmail.com>
2024-02-05 22:23:21 -0700

    rebuild

--- app.vue
+++ app.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <Head>
-      <link rel="stylesheet" href="https://unpkg.com/bamboo.css">
-      <meta name="theme-color" content="#684191" />
-      <meta name="description" content="Testing my PWA" />
+      <Link rel="stylesheet" href="https://unpkg.com/bamboo.css" />
+      <Meta name="theme-color" content="#684191" />
+      <Meta name="description" content="Testing my PWA" />
     </Head>
     <VitePwaManifest />
     <NuxtPage />
